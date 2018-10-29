e9e5df2ecb340b7f796d54510e90654f5faf5483
JakeOlsenBSU <35262965+JakeOlsenBSU@users.noreply.github.com>
2018-10-29 17:03:51 -0600

    Updated Course.java
    Changed instance variables to more useful names
    Constructor in place
    Conflict check still WIP

--- Conflict Tool/src/Course.java
+++ Conflict Tool/src/Course.java
@@ -1,45 +1,90 @@
+import java.sql.Time;
 
 public class Course {
 
+	private int rowID;
 	private String subject;
-	private String description;
-	private String classSection;
-	private String meetingTime;
+	private Time startTime;
+	private Time endTime;
+	private String days;
 	private String instructor;
 	private String location;
-	private String room;
 	
-	Course(){
-		
+	Course(int rowNumber,String subject,Time start, Time end, String days,String instructor,String location){
+		this.rowID = rowNumber;
+		this.subject = subject;
+		this.startTime = start;
+		this.endTime = end;
+		this.days = days;
+		this.instructor = instructor;
+		this.location = location;
 		
 	}
 	
-	private String getSubject(){
-		
+	public int getRowID() {
+		return rowID;
+	}
+
+	public void setRowID(int rowID) {
+		this.rowID = rowID;
+	}
+	
+	public String getSubject() {
 		return subject;
 	}
-	
-	private String getDescription(){
-		return description;
+
+	public void setSubject(String subject) {
+		this.subject = subject;
 	}
 	
-	private String getclassSection(){
-		return classSection;
+	public Time getStartTime() {
+		return startTime;
 	}
-	
-	String getMeetingTime(){
-		return meetingTime;
+
+	public void setStartTime(Time startTime) {
+		this.startTime = startTime;
 	}
-	
-	private String getInstructor(){
+
+	public Time getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Time endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getDays() {
+		return days;
+	}
+
+	public void setDays(String days) {
+		this.days = days;
+	}
+
+	public String getInstructor() {
 		return instructor;
 	}
-	
-	private String getLocation(){
+
+	public void setInstructor(String instructor) {
+		this.instructor = instructor;
+	}
+
+	public String getLocation() {
 		return location;
 	}
-	
-	private String getRoom(){
-		return room;
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public boolean conflict(Course checkCourse) {
+		// Needs work, just checks professor against time start time and days
+		// proof of concept, full Conflict checking WIP
+		if (this.instructor.equals(checkCourse.getInstructor()) && this.startTime.equals(checkCourse.getStartTime()) && this.days.equals(checkCourse.getDays()) && this.rowID != checkCourse.getRowID()) {
+			return true;
+		}
+		return false;
+	}
+	public String toString(){
+		return ( rowID + " " + subject + " -- " + instructor + " -- " + location + " -- " + startTime +" to "+ endTime + " on " + days + "\n");
 	}
 }
