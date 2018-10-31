e8b849fdae6cb1566d46d80cd02b0e30a667b659
JakeOlsenBSU <35262965+JakeOlsenBSU@users.noreply.github.com>
2018-10-31 13:54:26 -0600

    Update Course.java
    Added Methods:
    graduateLevel
    timeCheck
    sameDay
    and several instance variables to facilitate proper conflict searching

--- Conflict Tool/src/Course.java
+++ Conflict Tool/src/Course.java
@@ -3,19 +3,25 @@ import java.sql.Time;
 public class Course {
 
 	private int rowID;
+	private int nbr;
 	private String subject;
 	private Time startTime;
 	private Time endTime;
+	private String dates;
 	private String days;
+	private int[] dayArray;
 	private String instructor;
 	private String location;
 	
-	Course(int rowNumber,String subject,Time start, Time end, String days,String instructor,String location){
+	Course(int rowNumber,Double nbr,String subject,Time start, Time end,String dates, String days,int[] dayArray,String instructor,String location){
 		this.rowID = rowNumber;
+		this.nbr = (int) Math.round(nbr);
 		this.subject = subject;
 		this.startTime = start;
 		this.endTime = end;
+		this.dates = dates;
 		this.days = days;
+		this.dayArray = dayArray;
 		this.instructor = instructor;
 		this.location = location;
 		
@@ -29,6 +35,14 @@ public class Course {
 		this.rowID = rowID;
 	}
 	
+	public int getNbr() {
+		return nbr;
+	}
+
+	public void setNbr(int nbr) {
+		this.nbr = nbr;
+	}
+
 	public String getSubject() {
 		return subject;
 	}
@@ -53,12 +67,28 @@ public class Course {
 		this.endTime = endTime;
 	}
 
+	public String getDates() {
+		return dates;
+	}
+
+	public void setDates(String dates) {
+		this.dates = dates;
+	}
+
 	public String getDays() {
 		return days;
 	}
 
 	public void setDays(String days) {
 		this.days = days;
+	}
+
+	public int[] getDayArray() {
+		return dayArray;
+	}
+
+	public void setDayArray(int[] dayArray) {
+		this.dayArray = dayArray;
 	}
 
 	public String getInstructor() {
@@ -79,12 +109,44 @@ public class Course {
 	public boolean conflict(Course checkCourse) {
 		// Needs work, just checks professor against time start time and days
 		// proof of concept, full Conflict checking WIP
-		if (this.instructor.equals(checkCourse.getInstructor()) && this.startTime.equals(checkCourse.getStartTime()) && this.days.equals(checkCourse.getDays()) && this.rowID != checkCourse.getRowID()) {
+		if (this.getInstructor().equals(checkCourse.getInstructor()) && this.getRowID() != checkCourse.getRowID() && !this.getInstructor().equals("STAFF")) {
+			if (timeCheck(checkCourse)) {
+				return true;	
+			}
+		}
+		if (this.getLocation().equals(checkCourse.getLocation()) && this.getRowID() != checkCourse.getRowID() && this.getLocation().length() > 2) {
+			if (timeCheck(checkCourse)) {
+				return true;	
+			}
+		}
+		return false;
+	}
+	public boolean timeCheck(Course checkCourse) {
+		if (this.getStartTime().equals(checkCourse.getStartTime()) && this.sameDay(checkCourse) && this.dates.equals(checkCourse.getDates()) ||
+			this.getEndTime().equals(checkCourse.getEndTime()) && this.sameDay(checkCourse) && this.dates.equals(checkCourse.getDates()) ||
+			this.getStartTime().before(checkCourse.getStartTime()) && this.getEndTime().after(checkCourse.getEndTime())  && this.sameDay(checkCourse) && this.dates.equals(checkCourse.getDates()) ||
+			this.getEndTime().after(checkCourse.getStartTime()) && this.getStartTime().before(checkCourse.getEndTime())  && this.sameDay(checkCourse) && this.dates.equals(checkCourse.getDates())) {
 			return true;
 		}
 		return false;
 	}
+	public boolean sameDay(Course checkCourse) {
+		for (int i=0;i<4;i++) {
+			if(this.dayArray[i] == checkCourse.getDayArray()[i] && this.dayArray[i] == 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean graduateLevel(Course checkCourse) {
+		if (this.getNbr() % 100 == checkCourse.getNbr() % 100 && (this.getNbr() > 500 || checkCourse.getNbr() > 500)) {
+			return true;
+		}
+		return false;
+	}
+	
 	public String toString(){
-		return ( rowID + " " + subject + " -- " + instructor + " -- " + location + " -- " + startTime +" to "+ endTime + " on " + days + "\n");
+		return ( rowID + " " + nbr + " - " + subject + " -- " + instructor + " -- " + location + " -- " + startTime +" to "+ endTime + " on " + days +  " - " + dates +"\n");
 	}
 }
