d467a6b24ebb6f506fcd8455bf792c25e6304d6d
David Tovar <davidtovar2@u.boisestate.edu>
2018-10-31 23:46:35 -0600

    fixed Filechooser bug

--- Conflict Tool/src/Course.java
+++ Conflict Tool/src/Course.java
@@ -1,45 +1,155 @@
 
+import java.sql.Time;
+
+import javax.swing.JFileChooser;
+
 public class Course {
 
+	private int rowID;
+	private int nbr;
 	private String subject;
-	private String description;
-	private String classSection;
-	private String meetingTime;
+	private Time startTime;
+	private Time endTime;
+	private String dates;
+	private String days;
+	private int[] dayArray;
 	private String instructor;
 	private String location;
-	private String room;
 	
-	Course(){
-		
+	Course(int rowNumber,Double nbr,String subject,Time start, Time end,String dates, String days,int[] dayArray,String instructor,String location){
+		this.rowID = rowNumber;
+		this.nbr = (int) Math.round(nbr);
+		this.subject = subject;
+		this.startTime = start;
+		this.endTime = end;
+		this.dates = dates;
+		this.days = days;
+		this.dayArray = dayArray;
+		this.instructor = instructor;
+		this.location = location;
 		
 	}
 	
-	private String getSubject(){
-		
+	public int getRowID() {
+		return rowID;
+	}
+
+	public void setRowID(int rowID) {
+		this.rowID = rowID;
+	}
+	
+	public int getNbr() {
+		return nbr;
+	}
+
+	public void setNbr(int nbr) {
+		this.nbr = nbr;
+	}
+
+	public String getSubject() {
 		return subject;
 	}
-	
-	private String getDescription(){
-		return description;
+
+	public void setSubject(String subject) {
+		this.subject = subject;
 	}
 	
-	private String getclassSection(){
-		return classSection;
+	public Time getStartTime() {
+		return startTime;
 	}
-	
-	String getMeetingTime(){
-		return meetingTime;
+
+	public void setStartTime(Time startTime) {
+		this.startTime = startTime;
 	}
-	
-	private String getInstructor(){
+
+	public Time getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Time endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getDates() {
+		return dates;
+	}
+
+	public void setDates(String dates) {
+		this.dates = dates;
+	}
+
+	public String getDays() {
+		return days;
+	}
+
+	public void setDays(String days) {
+		this.days = days;
+	}
+
+	public int[] getDayArray() {
+		return dayArray;
+	}
+
+	public void setDayArray(int[] dayArray) {
+		this.dayArray = dayArray;
+	}
+
+	public String getInstructor() {
 		return instructor;
 	}
-	
-	private String getLocation(){
+
+	public void setInstructor(String instructor) {
+		this.instructor = instructor;
+	}
+
+	public String getLocation() {
 		return location;
 	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public boolean conflict(Course checkCourse) {
+		// Needs work, just checks professor against time start time and days
+		// proof of concept, full Conflict checking WIP
+		if (this.getInstructor().equals(checkCourse.getInstructor()) && this.getRowID() != checkCourse.getRowID() && !this.getInstructor().equals("STAFF")) {
+			if (timeCheck(checkCourse)) {
+				return true;	
+			}
+		}
+		if (this.getLocation().equals(checkCourse.getLocation()) && this.getRowID() != checkCourse.getRowID() && this.getLocation().length() > 2) {
+			if (timeCheck(checkCourse)) {
+				return true;	
+			}
+		}
+		return false;
+	}
+	public boolean timeCheck(Course checkCourse) {
+		if (this.getStartTime().equals(checkCourse.getStartTime()) && this.sameDay(checkCourse) && this.dates.equals(checkCourse.getDates()) ||
+			this.getEndTime().equals(checkCourse.getEndTime()) && this.sameDay(checkCourse) && this.dates.equals(checkCourse.getDates()) ||
+			this.getStartTime().before(checkCourse.getStartTime()) && this.getEndTime().after(checkCourse.getEndTime())  && this.sameDay(checkCourse) && this.dates.equals(checkCourse.getDates()) ||
+			this.getEndTime().after(checkCourse.getStartTime()) && this.getStartTime().before(checkCourse.getEndTime())  && this.sameDay(checkCourse) && this.dates.equals(checkCourse.getDates())) {
+			return true;
+		}
+		return false;
+	}
+	public boolean sameDay(Course checkCourse) {
+		for (int i=0;i<4;i++) {
+			if(this.dayArray[i] == checkCourse.getDayArray()[i] && this.dayArray[i] == 1) {
+				return true;
+			}
+		}
+		return false;
+	}
 	
-	private String getRoom(){
-		return room;
+	public boolean graduateLevel(Course checkCourse) {
+		if (this.getNbr() % 100 == checkCourse.getNbr() % 100 && (this.getNbr() > 500 || checkCourse.getNbr() > 500)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public String toString(){
+		return ( rowID + " " + nbr + " - " + subject + " -- " + instructor + " -- " + location + " -- " + startTime +" to "+ endTime + " on " + days +  " - " + dates +"\n");
 	}
 }
--- Conflict Tool/src/SchedulingToolPanel.java
+++ Conflict Tool/src/SchedulingToolPanel.java
@@ -1,5 +1,8 @@
 import java.awt.*;
 import java.io.*;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,10 +11,12 @@ import javax.swing.*;
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFFont;
@@ -26,38 +31,36 @@ public class SchedulingToolPanel extends JPanel implements ActionListener {
 	private JPanel buttonPanel;
 	private JButton importButton;
 	private JButton exportButton;
-	private JButton csvButton;
+	private JButton constraintsButton;
 	private JTextArea textField;
 	private JTextArea textFieldReport;
+	private File constraints;
+	private ArrayList<Course> courseList = new ArrayList<Course>();
 	
 	SchedulingToolPanel(){
 	
+		constraints = new File("resources/textConstraints.txt");
 		//Panel For Text Fields
 		textFieldPanel = new JPanel();
 		textFieldPanel.setBackground(Color.darkGray);
+		textFieldPanel.setLayout(new BorderLayout(5,5));
 		
 		textField = new JTextArea();
-		textField.setPreferredSize(new Dimension(400,500));
+		textField.setPreferredSize(new Dimension(600,400));
 		textField.setEditable(true);
 		textField.setLineWrap(true);
-		textField.setAutoscrolls(true);
-		textField.setText("Constraints");
-		
-		int tfHeight = textField.getHeight();
-		int tfWidth = textField.getWidth();
+	
 		
 		textFieldReport = new JTextArea();
-		//textFieldReport.setPreferredSize(new Dimension(400, 500));
-		textFieldReport.setBounds(0, 510, 
-				tfWidth, tfHeight - (tfHeight - 20));
-		textFieldReport.setText("Report Generated");
+		textFieldReport.setPreferredSize(new Dimension(400, 50));
+		textFieldReport.setText("Please Select a File - Import");
 		textFieldReport.setEditable(false);
 		
 //		JScrollPane scroll = new JScrollPane (textField);
 //		scroll.setPreferredSize(new Dimension(10,200));
 //		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		textFieldPanel.add(textField);
-		textFieldPanel.add(textFieldReport);
+		textFieldPanel.add(textField,BorderLayout.CENTER);
+		textFieldPanel.add(textFieldReport,BorderLayout.SOUTH);
 		
 		//Panel For Buttons
 		buttonPanel = new JPanel();
@@ -65,18 +68,30 @@ public class SchedulingToolPanel extends JPanel implements ActionListener {
 		
 		importButton = new JButton("Import");
 		exportButton = new JButton("Export");
-		csvButton = new JButton("CSV");
+		constraintsButton = new JButton("Constraints");
+		buttonPanel.add(constraintsButton);
 		buttonPanel.add(importButton);
 		buttonPanel.add(exportButton);
-		buttonPanel.add(csvButton);
 		
-		this.setLayout(new BorderLayout());
+		JPanel emptyPanel = new JPanel();
+		emptyPanel.setBackground(Color.DARK_GRAY);
+		
+		JPanel emptyPanel1 = new JPanel();
+		emptyPanel1.setBackground(Color.DARK_GRAY);
+		JPanel emptyPanel2 = new JPanel();
+		emptyPanel2.setBackground(Color.DARK_GRAY);
+		
+		this.setBackground(Color.DARK_GRAY);
+		this.setLayout(new BorderLayout(5,5));
 		this.add(textFieldPanel, BorderLayout.CENTER);
 		this.add(buttonPanel, BorderLayout.SOUTH);
-		
+		this.add(emptyPanel, BorderLayout.EAST);
+		this.add(emptyPanel1, BorderLayout.WEST);
+		this.add(emptyPanel2, BorderLayout.NORTH);
 		
 		importButton.addActionListener(this);
 		exportButton.addActionListener(this);
+		constraintsButton.addActionListener(this);
 		
 	}
 
@@ -88,16 +103,18 @@ public class SchedulingToolPanel extends JPanel implements ActionListener {
 		JButton src = (JButton) e.getSource();
 		
 		if (src == importButton){
-			
+			if(chooseFile() != null){
 			chooseFile();
 			loadConstraints();	
+			}
 			
 		} else if (src == exportButton){
-	
 			//test();
 			updateWorkbook();
-		}else if( src == csvButton){
-			
+		}else if(src == constraintsButton) {
+			if(constraintsFile() != null){
+			constraints = constraintsFile();
+			}
 		}
 		
 		
@@ -106,10 +123,33 @@ public class SchedulingToolPanel extends JPanel implements ActionListener {
 	private File chooseFile(){
 		
 		final JFileChooser fc = new JFileChooser();
+		int status = fc.showOpenDialog(this);
+
+		if(status == JFileChooser.CANCEL_OPTION){
+			return null;
+		} else if(status == JFileChooser.APPROVE_OPTION){
+		textFieldReport.setText(fc.getSelectedFile().getPath());
+		}
+
+		return fc.getSelectedFile();
+		
+	}
+	
+	// opens Constraints file location.
+	private File constraintsFile(){
+		
+		final JFileChooser fc = new JFileChooser();
+		int status = fc.showOpenDialog(this);
+		
+		if(status == JFileChooser.CANCEL_OPTION){
+			return null;
+		} else if(status == JFileChooser.APPROVE_OPTION){
+		File constraintsArea = new File("C:\\Users\\davidtovar\\eclipse-workspace\\Conflict tool\\Resources");
+		fc.setCurrentDirectory(constraintsArea);
 		fc.showOpenDialog(this);
 		
-		textFieldReport.setText(fc.getSelectedFile().getPath());
-		
+		textFieldReport.setText("New Constraints File:\n" + fc.getSelectedFile().getPath());
+		}
 		return fc.getSelectedFile();
 		
 	}
@@ -117,9 +157,8 @@ public class SchedulingToolPanel extends JPanel implements ActionListener {
 	private void loadConstraints(){
 		
 		FileReader fr = null;
-		File file = new File("resources/textConstraints.txt");
 		try {
-			fr = new FileReader(file);
+			fr = new FileReader(constraints);
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -139,107 +178,68 @@ public class SchedulingToolPanel extends JPanel implements ActionListener {
 		}
 			textField.setText(b.toString());
 	}
-
-	public void test(){
-		XSSFWorkbook workbook = new XSSFWorkbook();
-		
-		XSSFSheet sheet = workbook.createSheet("Persons");
-		sheet.setColumnWidth(0, 6000);
-		sheet.setColumnWidth(1, 4000);
-		 
-		XSSFRow header = sheet.createRow(0);
-		 
-		XSSFCellStyle headerStyle = workbook.createCellStyle();
-		headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
-		headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-		 
-		XSSFFont font = ((XSSFWorkbook) workbook).createFont();
-		font.setFontName("Arial");
-		font.setFontHeightInPoints((short) 16);
-		font.setBold(true);
-		headerStyle.setFont(font);
-		 
-		XSSFCell headerCell = header.createCell(0);
-		headerCell.setCellValue("Name");
-		headerCell.setCellStyle(headerStyle);
-		 
-		headerCell = header.createCell(1);
-		headerCell.setCellValue("Age");
-		headerCell.setCellStyle(headerStyle);
-		
-		//File currDir = new File(".");
-		//String path = currDir.getAbsolutePath();
-		String fileLocation = "/Users/davidtovar/Documents/" + "temp.xlsx"; 
-	
-		FileOutputStream outputStream = null;
-		try {
-			outputStream = new FileOutputStream(fileLocation);
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		try {
-			workbook.write(outputStream);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		try {
-			workbook.close();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-	}
 	
 	public void updateWorkbook(){
 		
 		try {
 			
-		FileInputStream inputStream = new FileInputStream(new File("/Users/davidtovar/Downloads/Spring 2019 Validation Report Example (1).xlsx"));
+		FileInputStream inputStream = new FileInputStream(new File("/Users/davidtovar/Downloads/Spring 2019 Validation Report Example.xlsx"));
         XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
-		//XSSFWorkbook workbook = (XSSFWorkbook) WorkbookFactory.create(inputStream);
         
         XSSFSheet sheet = workbook.getSheetAt(0);
-        
+		
         int maxColumn = 0;
-        int columnToDelete = 0;
-        for ( int r=0; r < sheet.getLastRowNum()+1; r++ ){
+        int[] columnDeleteIndex = {31,30,29,28,27,26,25,23,22,21,20,18,15,14,13,12,11,10,9,8,6,5,4,3};
+        for(int columnToDelete:columnDeleteIndex) {
+        	for ( int r=0; r < sheet.getLastRowNum()+1; r++ ){
             
-        	XSSFRow row = sheet.getRow( r );
+        		XSSFRow row = sheet.getRow( r );
             
-            if ( row == null )
-                continue;
+        		if ( row == null )
+        			continue;
             
-            int lastColumn = row.getLastCellNum();
-            if ( lastColumn > maxColumn )
-                maxColumn = lastColumn;
+        		int lastColumn = row.getLastCellNum();
+        		if ( lastColumn > maxColumn )
+        			maxColumn = lastColumn;
             
-            if ( lastColumn < columnToDelete )
-                continue;
+        		if ( lastColumn < columnToDelete )
+        			continue;
             
-            for ( int x=columnToDelete+1; x < lastColumn + 1; x++ ){
-                XSSFCell oldCell = row.getCell(x-1);
-                if ( oldCell != null )
-                    row.removeCell( oldCell );
+        		for ( int x=columnToDelete+1; x < lastColumn + 1; x++ ){
+        			XSSFCell oldCell = row.getCell(x-1);
+        			if ( oldCell != null )
+        				row.removeCell( oldCell );
                 	
-                XSSFCell nextCell = row.getCell( x );
-                if ( nextCell != null ){
-                    @SuppressWarnings("deprecation")
-					XSSFCell newCell = row.createCell(x-1, nextCell.getCellType());
-                    cloneCell(newCell, nextCell);
-                }
-            }
-            
-            inputStream.close();
-            
-            FileOutputStream outputStream = new FileOutputStream("/Users/davidtovar/Documents/" + "temp.xlsx");
-            workbook.write(outputStream);
-           // workbook.close();
-            outputStream.close();
-            
+        			XSSFCell nextCell = row.getCell( x );
+        			if ( nextCell != null ){
+        				@SuppressWarnings("deprecation")
+        				XSSFCell newCell = row.createCell(x-1, nextCell.getCellType());
+        				cloneCell(newCell, nextCell);
+        			}
+        		}
+        	}
         }
+        inputStream.close();
         
+        // runs through sheet, adding all rows that have time slots as courses into CourseList
+        sheet.createFreezePane(0, 1);
+        for ( int r=0; r < sheet.getLastRowNum(); r++ ) {
+            sheet.autoSizeColumn(r);
+        }
+        for (Row myRow: sheet) {
+    		if (myRow.getCell(4) == null || myRow.getRowNum() == 0) {
+    		}else {	
+            	courseList.add(addCourse(myRow));
+        	}
+        }
+        checkProfessorsRooms(courseList,workbook);
+        
+        FileOutputStream outputStream = new FileOutputStream("/Users/davidtovar/Documents/" + "temp.xlsx");
+        workbook.write(outputStream);
+//        workbook.close();
+        textFieldReport.setText("New Export File:\n" + "C:\\Users\\davidtovar\\Documents\\" + "temp.xlsx");
+        outputStream.close();
+
 		} catch (IOException | EncryptedDocumentException e){
 			// TODO
 			e.printStackTrace();
@@ -266,4 +266,78 @@ public class SchedulingToolPanel extends JPanel implements ActionListener {
 	        }
 
 	    }
+	
+	// Breaks down time Slot value down to individual hours and minutes so that it can be
+	// entered into a the Course as a java Time value.  Should account for pm values.
+	// Currently prints the course to be added to the console for error checking.
+	private static Course addCourse(Row newCourse) {
+			char[] timeBreak = newCourse.getCell(4).toString().toCharArray();
+			int startHour = timeBreak[1]-'0' + (timeBreak[0]-'0')*10;
+			int endHour = timeBreak[10]-'0' + (timeBreak[9]-'0')*10;
+			int startMinute = timeBreak[4]-'0' + (timeBreak[3]-'0')*10;
+			int endMinute = timeBreak[13]-'0' + (timeBreak[12]-'0')*10;
+			int[] dayArray = new int[5];
+			String days = null;
+			StringTokenizer daysBreak = new StringTokenizer(newCourse.getCell(4).toString());
+			while (daysBreak.hasMoreElements()) {
+				days = (String)daysBreak.nextElement();
+			}
+			if (timeBreak[6] == 'p' && startHour != 12) {
+				startHour+=12;
+			}
+			if (timeBreak[15] == 'p' && endHour != 12) {
+				endHour+=12;
+			}
+			if (days.contains("Mo")) {dayArray[0] = 1;}
+			if (days.contains("Tu")) {dayArray[1] = 1;}
+			if (days.contains("We")) {dayArray[2] = 1;}
+			if (days.contains("Th")) {dayArray[3] = 1;}
+			if (days.contains("Fr")) {dayArray[4] = 1;}
+			
+			Time startTime = new Time(startHour, startMinute, 0);
+			Time endTime = new Time(endHour, endMinute, 0);
+			Course course = new Course(newCourse.getRowNum(),newCourse.getCell(1).getNumericCellValue(),newCourse.getCell(2).toString(), startTime, endTime,newCourse.getCell(5).toString(), days, dayArray, newCourse.getCell(7).toString(), newCourse.getCell(6).toString());
+			System.out.print(course.toString());
+			return course;
+	}
+	
+	// Takes courseList and workbook for comparisons and highlighting fields that are in Conflict.
+	private static void checkProfessorsRooms(ArrayList<Course> courseList,XSSFWorkbook workbook) {
+		
+		XSSFSheet sheet = workbook.getSheetAt(0);
+		XSSFCellStyle style = workbook.createCellStyle();
+		XSSFFont font = workbook.createFont();
+		font.setColor(IndexedColors.RED.getIndex());
+		font.setBold(true);
+		style.setFont(font);
+		
+		XSSFCellStyle style2 = workbook.createCellStyle();
+		XSSFFont font2 = workbook.createFont();
+		font2.setColor(IndexedColors.BLUE.getIndex());
+		font2.setBold(true);
+		style2.setFont(font2);
+		
+		for(Course currentCourse:courseList) {
+			for(Course checkCourse:courseList) {
+				if (currentCourse.conflict(checkCourse)) {
+					if (currentCourse.graduateLevel(checkCourse)) {
+						Row conflictRow = sheet.getRow(currentCourse.getRowID());
+        				Cell conflictCell = conflictRow.createCell(conflictRow.getLastCellNum());
+        				conflictCell.setCellValue("Potential Conflict Or Graduate Class");
+	            		for (Cell testcell : conflictRow){
+	        				testcell.setCellStyle(style2);
+
+	            		}	
+					} else {
+						Row conflictRow = sheet.getRow(currentCourse.getRowID());
+        				Cell conflictCell = conflictRow.createCell(conflictRow.getLastCellNum());
+        				conflictCell.setCellValue("Conflict");
+	            		for (Cell testcell : conflictRow){
+	        				testcell.setCellStyle(style);
+	            		}	
+					}
+				}
+			}
+		}
+	}
 }
